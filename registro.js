f77b99574b8e9f99ee509c15534b49e49b9f31b8
AndresMaltes <123115083+AndresMaltes@users.noreply.github.com>
2023-06-07 11:40:20 -0500

    Add files via upload

---
+++ registro.js
@@ -0,0 +1,170 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBFyQXP99eBMj0sQuABfcr7sHQn51ywE7U",
+    authDomain: "arkitask2.firebaseapp.com",
+    databaseURL: "https://arkitask2-default-rtdb.firebaseio.com",
+    projectId: "arkitask2",
+    storageBucket: "arkitask2.appspot.com",
+    messagingSenderId: "558206073756",
+    appId: "1:558206073756:web:aa50c3e2f4d341fbcde659",
+    measurementId: "G-1KM9F1VB5F"
+  };
+//Referencias
+const emailbox = document.getElementById("correo");
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const userRef = firebase.database().ref('Usuarios/')
+//Insercion de datos
+function insertData(){
+    const userbox = document.getElementById("usuario");
+    const namebox = document.getElementById("nombre");
+    const passwordbox = document.getElementById("password");
+    const repeatPasswordbox = document.getElementById("password2");
+    
+    const telefonobox = document.getElementById("telefono");
+    const rolbox = document.getElementById('rol')
+    
+    
+    const registerUser = userRef.push();
+    registerUser.set({
+        User: userbox,
+        Nombre: namebox,
+        Password: passwordbox,
+        Confirm_Password: repeatPasswordbox,
+        Correo: emailbox,
+        Rol: rolbox
+    })
+    console.log("exito");
+}
+
+const selectData = async (e) => {
+    await userRef.on('value', (students) => {
+        students.forEach((student) =>{
+            let studentData = student.val()
+            if ("allan@gmail.com" == studentData.Correo) {
+                console.log("Correo correcto");
+            }else{
+                console.log(studentData.Correo);
+            }
+        })
+    })
+}
+
+
+
+var insBtn = document.querySelector(".formulario__btn");
+
+insBtn.addEventListener('click', selectData)
+// var selBtn = document.getElementById("Selbtn");
+// var updBtn = document.getElementById("Updbtn");
+// var delBtn = document.getElementById("Delbtn");
+
+
+
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+const campos = {
+	usuario: false,
+	nombre: false,
+	password: false,
+	correo: false,
+	telefono: false
+}
+
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "usuario":
+			validarCampo(expresiones.usuario, e.target, 'usuario');
+		break;
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+		case "password":
+			validarCampo(expresiones.password, e.target, 'password');
+			validarPassword2();
+		break;
+		case "password2":
+			validarPassword2();
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+		case "telefono":
+			validarCampo(expresiones.telefono, e.target, 'telefono');
+		break;
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
+}
+
+const validarPassword2 = () => {
+	const inputPassword1 = document.getElementById('password');
+	const inputPassword2 = document.getElementById('password2');
+
+	if(inputPassword1.value !== inputPassword2.value){
+		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos['password'] = false;
+	} else {
+		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos['password'] = true;
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const terminos = document.getElementById('terminos');
+	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
+		formulario.reset();
+
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+	} else {
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
+});
+  
